6f021859aef06f48e4a0f875851491d9a4aea7ea
Your Name <you@example.com>
2026-05-08 14:06:42 -0400

    task 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -5,13 +5,16 @@ def emotion_detector(text_to_analyze):
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = { "raw_document": { "text": text_to_analyze } }
     res = requests.post(url, json=input_json, headers=header)
-    if res.status_code == 200:
-        return res.json()
-    else:
+    if res.status_code != 200:
         return {"error": f"Request failed with status code {res.status_code}"}
+    output = json.loads(res.text)
+    emotions = output['emotionPredictions'][0]['emotion']
+    emotions['dominant_emotion'] = max(emotions, key=emotions.get)
+    return emotions
 
 
 if __name__ == "__main__":
     sample_text = "I love this new technology."
     result = emotion_detector(sample_text)
-    print(json.dumps(result))
+    print(result)
+
